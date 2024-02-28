7db9e9ee6e1971182ac21c6a3a0c0058e2cf68d2
Jeong Jeongil <jji042842@gmail.com>
2024-02-28 17:29:21 +0900

    Add request matcher list on posting detail

--- src/main/java/com/onetwo/postservice/common/config/SecurityConfig.java
+++ src/main/java/com/onetwo/postservice/common/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @EnableWebSecurity
@@ -19,7 +20,13 @@ public class SecurityConfig {
         return new RequestMatcher() {
             @Override
             public List<MvcRequestMatcher> getMvcRequestMatcherArray() {
-                return List.of(mvc.pattern(HttpMethod.GET, GlobalUrl.POSTING_FILTER));
+                List<MvcRequestMatcher> mvcRequestMatcherList = new ArrayList<>();
+
+                mvcRequestMatcherList.add(mvc.pattern(HttpMethod.GET, GlobalUrl.POSTING_FILTER));
+
+                mvcRequestMatcherList.add(mvc.pattern(HttpMethod.GET, GlobalUrl.POSTING_ROOT + GlobalUrl.EVERY_UNDER_ROUTE));
+
+                return mvcRequestMatcherList;
             }
         };
     }
