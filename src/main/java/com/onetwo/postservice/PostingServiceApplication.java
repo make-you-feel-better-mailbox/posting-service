c3a8b0d7484146e6d7fa0625489f203e2fe4a711
Jeong Jeongil <jji042842@gmail.com>
2024-04-24 14:37:39 +0900

    config on active property

--- src/main/java/com/onetwo/postservice/PostingServiceApplication.java
+++ src/main/java/com/onetwo/postservice/PostingServiceApplication.java
@@ -1,5 +1,6 @@
 package com.onetwo.postservice;
 
+import com.onetwo.postservice.common.GlobalStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +10,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class PostingServiceApplication {
 
     public static void main(String[] args) {
+        String active = System.getProperty(GlobalStatus.SPRING_PROFILES_ACTIVE);
+        if (active == null) {
+            System.setProperty(GlobalStatus.SPRING_PROFILES_ACTIVE, GlobalStatus.SPRING_PROFILES_ACTIVE_DEFAULT);
+        }
+
+        System.setProperty(
+                GlobalStatus.SPRING_PROFILES_ACTIVE,
+                System.getProperty(GlobalStatus.SPRING_PROFILES_ACTIVE, GlobalStatus.SPRING_PROFILES_ACTIVE_DEFAULT)
+        );
         SpringApplication.run(PostingServiceApplication.class, args);
     }
 
